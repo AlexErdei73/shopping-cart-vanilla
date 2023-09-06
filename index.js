01d2f15bc49ca3ada06dfe03a05b746974d7f986
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-09-06 10:02:39 +0100

    Make app back button work

--- index.js
+++ index.js
@@ -5,23 +5,41 @@ const nodes = [];
 
 const templates = document.querySelectorAll("template");
 templates.forEach((temp, i) => {
-	const component = temp.content.querySelector(`.${components[i]}`);
-	nodes[i] = document.importNode(component, true);
+  const component = temp.content.querySelector(`.${components[i]}`);
+  nodes[i] = document.importNode(component, true);
 });
 
 main.appendChild(nodes[0]);
 
 const navButtons = document.querySelectorAll("nav button");
 
+function updateDOM(index) {
+  const childNode = main.childNodes[0];
+  childNode.remove();
+  main.appendChild(nodes[index]);
+}
+
 function handleNavButtonClick(event) {
-	const button = event.target;
-	const index = Array.from(navButtons).indexOf(button);
-	history.pushState({ index }, "", button.getAttribute("href"));
-	const childNode = main.childNodes[0];
-	childNode.remove();
-	main.appendChild(nodes[index]);
+  const button = event.target;
+  const index = Array.from(navButtons).indexOf(button);
+  history.pushState({ index }, "", button.getAttribute("href"));
+  updateDOM(index);
 }
 
 navButtons.forEach((navButton) =>
-	navButton.addEventListener("click", handleNavButtonClick)
+  navButton.addEventListener("click", handleNavButtonClick)
 );
+
+addEventListener("popstate", (event) => {
+  let index;
+  if (!event.state) index = 0;
+  else index = event.state.index;
+  updateDOM(index);
+});
+
+addEventListener("beforeunload", (event) => {
+  const URL = event.target.URL;
+  const component = URL.slice(URL.lastIndexOf("/") + 1);
+  const index = components.indexOf(component);
+  history.replaceState({ index }, "", "/");
+});
