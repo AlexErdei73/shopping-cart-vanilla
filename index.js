51c622c084bbe12970474fd5266aa1e3edb66b82
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-09-15 10:23:36 +0100

    Add state to index,js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import navigationBar from "./components/navigationBar.js";
 
+const state = {
+  books: [
+    {
+      title: "",
+      author: "",
+      pictureURL: "",
+      unitPrice: 0,
+      number: 0,
+      totalPrice: 0,
+    },
+  ],
+  totalPrice: 0,
+};
+
 navigationBar.showMainElement();
