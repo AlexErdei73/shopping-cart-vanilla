5bb6a1185d46f91194838d4f326b981ceaf71278
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-09-15 11:29:34 +0100

    Add book images to the app

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ import navigationBar from "./components/navigationBar.js";
 const state = {
   books: [
     {
-      title: "",
-      author: "",
-      pictureURL: "",
-      unitPrice: 0,
+      title: "Mathematical Mathods In The Physical Sciences",
+      author: "Mary L. Boas",
+      pictureURL: "../images/books/boas.b08a50df.jpeg",
+      unitPrice: 65,
+      number: 0,
+      totalPrice: 0,
+    },
+    {
+      title: "The Feynman Lectures On Physics",
+      author: "Richard P. Feynman",
+      pictureURL: "../images/books/feynman.jpeg",
+      unitPrice: 111.99,
       number: 0,
       totalPrice: 0,
     },
